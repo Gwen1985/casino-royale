aab91ddda79099f3df451033929b48ae38f75799
Gerrit Degenhardt <gerrit@degenhardt.be>
2020-08-07 12:24:47 +0200

    Get block coordinates

--- script/game_movingblock.js
+++ script/game_movingblock.js
@@ -28,15 +28,19 @@ window.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'ArrowLeft' :
             box.style.left = parseInt(box.style.left) - moveBy + 'px';
+            console.log(getCoordinates("#block"));
             break;
         case 'ArrowRight' :
             box.style.left = parseInt(box.style.left) + moveBy + 'px';
+            console.log(getCoordinates("#block"));
             break;
         case 'ArrowUp' :
             box.style.top = parseInt(box.style.top) - moveBy + 'px';
+            console.log(getCoordinates("#block"));
             break;
         case 'ArrowDown' :
             box.style.top = parseInt(box.style.top) + moveBy + 'px';
+            console.log(getCoordinates("#block"));
             break;
     }
 });
@@ -64,11 +68,60 @@ function makeNewPosition($game) {
 
 }
 
+function getCoordinates(element) {
+    let test = $(element);
+    let offsetElement = test.offset();
+    let top = offsetElement.top;
+    let left = Math.floor(offsetElement.left);
+    let bottom = top + 25;
+    let right = left + 25;
+
+    let coorElement = {
+        top: top,
+        right: right,
+        bottom: bottom,
+        left: left
+    };
+
+    return coorElement;
+}
+
+function checkHit() {
+    // let userBlockElement = document.getElementById(userBlock);
+    // let movingBlockElement = document.getElementById(movingBlock);
+    let userBlockCoor = getCoordinates("#block");
+    let movingBlockCoor1 = getCoordinates("#block_1");
+    // let movingBlockCoor2 = getCoordinates("#block_2");
+    // let movingBlockCoor3 = getCoordinates("#block_3");
+    // let movingBlockCoor4 = getCoordinates("#block_4");
+    //console.log(userBlockCoor.bottom, movingBlockCoor1.top);
+    //
+    // if (userBlockCoor.bottom === movingBlockCoor1.top || userBlockCoor.top === movingBlockCoor1.bottom) {
+    //     alert('Hit by BLOCK_1 BLUE');
+    // }
+    //
+    // if(userBlockCoor.bottom === movingBlockCoor2.top || userBlockCoor.top === movingBlockCoor2.bottom) {
+    //     alert('Hit by BLOCK_2 PURPLE');
+    // }
+    //
+    // if(userBlockCoor.bottom === movingBlockCoor3.top || userBlockCoor.top === movingBlockCoor3.bottom) {
+    //     alert('Hit by BLOCK_3 GREEN');
+    // }
+    //
+    // if(userBlockCoor.bottom === movingBlockCoor4.top || userBlockCoor.top === movingBlockCoor4.bottom) {
+    //     alert('Hit by BLOCK_4 YELLOW');
+    // }
+
+}
+
 function animateDiv(blockElemenet) {
     let $target = $(blockElemenet);
     let newq = makeNewPosition($target.parent());
     let oldq = $target.offset();
     let speed = 1100;//calcSpeed([oldq.top, oldq.left], newq);
+
+
+    checkHit();
 
     $(blockElemenet).animate({
         top: newq[0],
