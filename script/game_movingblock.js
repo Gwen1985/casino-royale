d9d9111f5ce2feb5151b1d4f588b653ddc6bf9f9
KimDelbarre <kim.delbarre@gmail.com>
2020-08-10 16:28:33 +0200

    change rules page moving block

--- script/game_movingblock.js
+++ script/game_movingblock.js
@@ -51,8 +51,6 @@
 // console.log(b);
 // console.log(coor);
 
-
-
 // window.addEventListener('keydown', (e) => {
 //     switch (e.key) {
 //         case 'ArrowLeft' :
@@ -74,189 +72,212 @@
 //     }
 // });
 
-let pane = $('#game'),
-    box = $('#block'),
-    w = pane.width() - box.width(),
-    d = {},
-    x = 5;
+// DOM VARIABLES
+let playerHeaderElement = document.getElementById("playerHeader"),
+  playerDeckElement = document.getElementById("playerDeck"),
+  playerScoreElement = document.getElementById("playerScore"),
+  dealerHeaderElement = document.getElementById("dealerHeader"),
+  dealerDeckElement = document.getElementById("dealerDeck"),
+  dealerScoreElement = document.getElementById("dealerScore"),
+  newgameButton = document.getElementById("newgame-button"),
+  hitButton = document.getElementById("hit-button"),
+  stayButton = document.getElementById("stay-button"),
+  descriptionElement = document.getElementById("description"),
+  gameElement = document.getElementById("game");
 
-function newv(v,a,b) {
-    let n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
-    return n < 0 ? 0 : n > w ? w : n;
+//GAME Js
+
+let pane = $("#game"),
+  box = $("#block"),
+  w = pane.width() - box.width(),
+  d = {},
+  x = 5;
+
+function newv(v, a, b) {
+  let n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+  return n < 0 ? 0 : n > w ? w : n;
 }
 
-$(window).keydown(function(e) { d[e.which] = true; });
-$(window).keyup(function(e) { d[e.which] = false; });
+$(window).keydown(function (e) {
+  d[e.which] = true;
+});
+$(window).keyup(function (e) {
+  d[e.which] = false;
+});
 
-setInterval(function() {
-    box.css({
-        left: function(i,v) { return newv(v, 37, 39); },
-        top: function(i,v) { return newv(v, 38, 40); }
-    });
+setInterval(function () {
+  box.css({
+    left: function (i, v) {
+      return newv(v, 37, 39);
+    },
+    top: function (i, v) {
+      return newv(v, 38, 40);
+    },
+  });
 }, 10);
 
 $(document).ready(function () {
-    disableScroll();
-    animateDiv('.block_1');
-    animateDiv('.block_2');
-    animateDiv('.block_3');
-    animateDiv('.block_4');
-
+  disableScroll();
+  animateDiv(".block_1");
+  animateDiv(".block_2");
+  animateDiv(".block_3");
+  animateDiv(".block_4");
 });
 
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
 
 function preventDefault(e) {
-    e.preventDefault();
+  e.preventDefault();
 }
 
 function preventDefaultForScrollKeys(e) {
-    if (keys[e.keyCode]) {
-        preventDefault(e);
-        return false;
-    }
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
 }
 
-    // Get viewport dimensions (remove the dimension of the div)
-    // $game = ($game || $(window));
-    // let h = $game.height() - 205;
-    // let w = $game.width() - 205;
+// Get viewport dimensions (remove the dimension of the div)
+// $game = ($game || $(window));
+// let h = $game.height() - 205;
+// let w = $game.width() - 205;
 
 // modern Chrome requires { passive: false } when adding event
 var supportsPassive = false;
 try {
-    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-        get: function () { supportsPassive = true; }
-    }));
-} catch(e) {}
+  window.addEventListener(
+    "test",
+    null,
+    Object.defineProperty({}, "passive", {
+      get: function () {
+        supportsPassive = true;
+      },
+    })
+  );
+} catch (e) {}
 
 var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+var wheelEvent =
+  "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
 
 // call this to Disable
 function disableScroll() {
-    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+  window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+  window.addEventListener("keydown", preventDefaultForScrollKeys, false);
 }
 
 // call this to Enable
 function enableScroll() {
-    window.removeEventListener('DOMMouseScroll', preventDefault, false);
-    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-    window.removeEventListener('touchmove', preventDefault, wheelOpt);
-    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+  window.removeEventListener("DOMMouseScroll", preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener("touchmove", preventDefault, wheelOpt);
+  window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
 }
-
-
 
 function getCoordinates(element) {
-    let test = $(element);
-    let offsetElement = test.offset();
-    let top = offsetElement.top;
-    let left = Math.floor(offsetElement.left);
-    let bottom = top + 25;
-    let right = left + 25;
+  let test = $(element);
+  let offsetElement = test.offset();
+  let top = offsetElement.top;
+  let left = Math.floor(offsetElement.left);
+  let bottom = top + 25;
+  let right = left + 25;
 
-    let coorElement = {
-        top: top,
-        // right: right,
-        // bottom: bottom,
-        left: left
-    };
+  let coorElement = {
+    top: top,
+    // right: right,
+    // bottom: bottom,
+    left: left,
+  };
 
-    return coorElement;
+  return coorElement;
 }
 
-function equalPositions(pos1, pos2){
-    console.log(pos1.top + '-' + pos2.top + ' - ' + pos1.left + '-' + pos2.left);
-    return (pos1.top === pos2.top);
+function equalPositions(pos1, pos2) {
+  console.log(pos1.top + "-" + pos2.top + " - " + pos1.left + "-" + pos2.left);
+  return pos1.top === pos2.top;
 }
 
 function checkHit() {
-    let userBlockElement = document.getElementById('block');
-    // let movingBlockElement = document.getElementById(movingBlock);
-    let userBlockCoor = getCoordinates("#block");
+  let userBlockElement = document.getElementById("block");
+  // let movingBlockElement = document.getElementById(movingBlock);
+  let userBlockCoor = getCoordinates("#block");
 
+  let movingBlockCoor1 = getCoordinates("#block_1");
 
-    let movingBlockCoor1 = getCoordinates("#block_1");
+  let temp = $("#block");
+  let offsetElement = temp.offset();
 
-    let temp = $('#block');
-    let offsetElement = temp.offset();
+  if (equalPositions(offsetElement, movingBlockCoor1)) {
+    alert("BAM");
+  }
 
-    if(equalPositions(offsetElement, movingBlockCoor1)) {
-        alert('BAM');
-    }
-
-
-    // let movingBlockCoor2 = getCoordinates("#block_2");
-    // let movingBlockCoor3 = getCoordinates("#block_3");
-    // let movingBlockCoor4 = getCoordinates("#block_4");
-    //console.log(userBlockCoor.bottom, movingBlockCoor1.top);
-    //
-    // if (userBlockCoor.bottom === movingBlockCoor1.top || userBlockCoor.top === movingBlockCoor1.bottom) {
-    //     alert('Hit by BLOCK_1 BLUE');
-    // }
-    //
-    // if(userBlockCoor.bottom === movingBlockCoor2.top || userBlockCoor.top === movingBlockCoor2.bottom) {
-    //     alert('Hit by BLOCK_2 PURPLE');
-    // }
-    //
-    // if(userBlockCoor.bottom === movingBlockCoor3.top || userBlockCoor.top === movingBlockCoor3.bottom) {
-    //     alert('Hit by BLOCK_3 GREEN');
-    // }
-    //
-    // if(userBlockCoor.bottom === movingBlockCoor4.top || userBlockCoor.top === movingBlockCoor4.bottom) {
-    //     alert('Hit by BLOCK_4 YELLOW');
-    // }
-
+  // let movingBlockCoor2 = getCoordinates("#block_2");
+  // let movingBlockCoor3 = getCoordinates("#block_3");
+  // let movingBlockCoor4 = getCoordinates("#block_4");
+  //console.log(userBlockCoor.bottom, movingBlockCoor1.top);
+  //
+  // if (userBlockCoor.bottom === movingBlockCoor1.top || userBlockCoor.top === movingBlockCoor1.bottom) {
+  //     alert('Hit by BLOCK_1 BLUE');
+  // }
+  //
+  // if(userBlockCoor.bottom === movingBlockCoor2.top || userBlockCoor.top === movingBlockCoor2.bottom) {
+  //     alert('Hit by BLOCK_2 PURPLE');
+  // }
+  //
+  // if(userBlockCoor.bottom === movingBlockCoor3.top || userBlockCoor.top === movingBlockCoor3.bottom) {
+  //     alert('Hit by BLOCK_3 GREEN');
+  // }
+  //
+  // if(userBlockCoor.bottom === movingBlockCoor4.top || userBlockCoor.top === movingBlockCoor4.bottom) {
+  //     alert('Hit by BLOCK_4 YELLOW');
+  // }
 }
 
 function animateDiv(blockElemenet) {
-    let $target = $(blockElemenet);
-    let newq = makeNewPosition($target.parent());
-    let oldq = $target.offset();
-    let speed = 1100;
-    //let speed = calcSpeed([oldq.top, oldq.left], newq);
+  let $target = $(blockElemenet);
+  let newq = makeNewPosition($target.parent());
+  let oldq = $target.offset();
+  let speed = 1100;
+  //let speed = calcSpeed([oldq.top, oldq.left], newq);
 
-    $(blockElemenet).animate({
-        top: newq[0],
-        left: newq[1]
-    }, speed, function () {
-        checkHit();
-        animateDiv(blockElemenet);
-    });
+  $(blockElemenet).animate(
+    {
+      top: newq[0],
+      left: newq[1],
+    },
+    speed,
+    function () {
+      checkHit();
+      animateDiv(blockElemenet);
+    }
+  );
 }
 
 function makeNewPosition($game) {
+  // Get viewport dimensions (remove the dimension of the div)
+  $game = $game || $(window);
+  let h = $game.height() - 200;
+  let w = $game.width() - 200;
 
-    // Get viewport dimensions (remove the dimension of the div)
-    $game = ($game || $(window));
-    let h = $game.height() - 200;
-    let w = $game.width() - 200;
+  let nh = Math.floor(Math.random() * h);
+  let nw = Math.floor(Math.random() * w);
 
-    let nh = Math.floor(Math.random() * h);
-    let nw = Math.floor(Math.random() * w);
-
-    return [nh, nw];
-
+  return [nh, nw];
 }
 
 function calcSpeed(prev, next) {
+  let x = Math.abs(prev[1] - next[1]);
+  let y = Math.abs(prev[0] - next[0]);
 
-    let x = Math.abs(prev[1] - next[1]);
-    let y = Math.abs(prev[0] - next[0]);
+  let greatest = x > y ? x : y;
 
-    let greatest = x > y ? x : y;
+  let speedModifier = 0.2;
 
-    let speedModifier = 0.2;
+  let speed = Math.ceil(greatest / speedModifier);
 
-    let speed = Math.ceil(greatest / speedModifier);
-
-    return speed;
-
+  return speed;
 }
-
